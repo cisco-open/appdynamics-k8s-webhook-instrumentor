6774db45d5a92cfda4c625b12d304cfdb9f96377
Martin <martin.divis@volny.cz>
2022-06-20 15:50:10 +0200

    Nodejs initial instrumentation

--- webhook/instrumentNodeJS.go
+++ webhook/instrumentNodeJS.go
@@ -1,9 +1,146 @@
 package main
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
 
 func nodejsInstrumentation(pod corev1.Pod, instrRule *InstrumentationRule) []patchOperation {
 	patchOps := []patchOperation{}
 
+	if len(pod.Spec.Containers) > 0 {
+		// fmt.Printf("Container Env: %d -> %v\n", len(pod.Spec.Containers[0].Env), pod.Spec.Containers[0].Env)
+		if len(pod.Spec.Containers[0].Env) == 0 {
+			patchOps = append(patchOps, patchOperation{
+				Op:    "add",
+				Path:  "/spec/containers/0/env",
+				Value: []corev1.EnvVar{},
+			})
+		}
+		if len(pod.Spec.Containers[0].VolumeMounts) == 0 {
+			patchOps = append(patchOps, patchOperation{
+				Op:    "add",
+				Path:  "/spec/containers/0/volumeMounts",
+				Value: []corev1.VolumeMount{},
+			})
+		}
+		if len(pod.Spec.Volumes) == 0 {
+			patchOps = append(patchOps, patchOperation{
+				Op:    "add",
+				Path:  "/spec/volumes/",
+				Value: []corev1.Volume{},
+			})
+		}
+		if len(pod.Spec.InitContainers) == 0 {
+			patchOps = append(patchOps, patchOperation{
+				Op:    "add",
+				Path:  "/spec/initContainers",
+				Value: []corev1.VolumeMount{},
+			})
+		}
+	}
+
+	patchOps = append(patchOps, addControllerEnvVars(0)...)
+	patchOps = append(patchOps, addNodejsEnvVar(pod, instrRule, 0)...)
+	patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_AGENT_APPLICATION_NAME", getApplicationName(pod, instrRule), 0))
+	patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_AGENT_TIER_NAME", getTierName(pod, instrRule), 0))
+	patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_AGENT_REUSE_NODE_NAME_PREFIX", getTierName(pod, instrRule), 0))
+	// not sure it has to be there, but ClusterAgent does the following, too
+	patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_AGENT_NODE_NAME", getTierName(pod, instrRule), 0))
+
+	patchOps = append(patchOps, addNetvizEnvVars(pod, instrRule, 0)...)
+
+	patchOps = append(patchOps, addNodejsAgentVolumeMount(pod, instrRule, 0)...)
+
+	patchOps = append(patchOps, addNodejsAgentInitContainer(pod, instrRule)...)
+
+	patchOps = append(patchOps, addNodejsAgentVolume(pod, instrRule)...)
+
+	return patchOps
+}
+
+func addNodejsEnvVar(pod corev1.Pod, instrRules *InstrumentationRule, containerIdx int) []patchOperation {
+	patchOps := []patchOperation{}
+
+	patchOps = append(patchOps, addContainerEnvVar("NODE_OPTIONS", "--require /opt/appdynamics-nodejs/shim.js", 0))
+	patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_AGENT_REUSE_NODE_NAME", "true", 0))
+
+	// Check for proxy settings, doc does not say anything
+	if config.ControllerConfig.UseProxy {
+		patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_PROXY_HOST_NAME", config.ControllerConfig.ProxyHost, 0))
+		patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_PROXY_PORT", config.ControllerConfig.ProxyPort, 0))
+		if config.ControllerConfig.ProxyUser != "" {
+			patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_PROXY_AUTH_NAME", config.ControllerConfig.ProxyUser, 0))
+			patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_PROXY_AUTH_PASSWORD", config.ControllerConfig.ProxyPassword, 0))
+		}
+		if config.ControllerConfig.ProxyDomain != "" {
+			patchOps = append(patchOps, addContainerEnvVar("APPDYNAMICS_PROXY_AUTH_DOMAIN", config.ControllerConfig.ProxyDomain, 0))
+		}
+	}
+
+	return patchOps
+}
+
+func addNodejsAgentVolumeMount(pod corev1.Pod, instrRules *InstrumentationRule, containerIdx int) []patchOperation {
+	patchOps := []patchOperation{}
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: fmt.Sprintf("/spec/containers/%d/volumeMounts/-", containerIdx),
+		Value: corev1.VolumeMount{
+			MountPath: "/opt/appdynamics-nodejs", //TODO
+			Name:      "appd-agent-repo-nodejs",  //TODO
+		},
+	})
+	return patchOps
+}
+
+func addNodejsAgentVolume(pod corev1.Pod, instrRules *InstrumentationRule) []patchOperation {
+	patchOps := []patchOperation{}
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: "/spec/volumes/-",
+		Value: corev1.Volume{
+			Name: "appd-agent-repo-nodejs", //TODO
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
+		},
+	})
+	return patchOps
+}
+
+func addNodejsAgentInitContainer(pod corev1.Pod, instrRules *InstrumentationRule) []patchOperation {
+	patchOps := []patchOperation{}
+	limCPU, _ := resource.ParseQuantity("200m")
+	limMem, _ := resource.ParseQuantity("75M")
+	reqCPU, _ := resource.ParseQuantity(instrRules.InjectionRules.ResourceReservation.CPU)
+	reqMem, _ := resource.ParseQuantity(instrRules.InjectionRules.ResourceReservation.Memory)
+
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: "/spec/initContainers/-",
+		Value: corev1.Container{
+			Name:            "appd-agent-attach-nodejs", //TODO
+			Image:           instrRules.InjectionRules.Image,
+			Command:         []string{"cp", "-r", "/opt/appdynamics/.", "/opt/appdynamics-nodejs"},
+			ImagePullPolicy: corev1.PullAlways, //TODO
+			Resources: corev1.ResourceRequirements{
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    limCPU,
+					corev1.ResourceMemory: limMem,
+				},
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    reqCPU,
+					corev1.ResourceMemory: reqMem,
+				},
+			},
+			VolumeMounts: []corev1.VolumeMount{{
+				MountPath: "/opt/appdynamics-nodejs", //TODO
+				Name:      "appd-agent-repo-nodejse", //TODO
+			}},
+		},
+	})
 	return patchOps
 }
