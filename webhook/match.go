29c8f9a4d0d21162ef6d855c359c2ee2e39f7de8
Martin <martin.divis@gmail.com>
2022-11-01 10:16:02 +0100

    Copyright Notice change to Cisco Systems Inc.

--- webhook/match.go
+++ webhook/match.go
@@ -1,11 +1,43 @@
+/*
+Copyright (c) 2019 Cisco Systems, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"regexp"
+	"text/template"
 
 	corev1 "k8s.io/api/core/v1"
 )
+
+func getFlexMatch(pod corev1.Pod, flexMatchTmpl *template.Template) string {
+
+	var res bytes.Buffer
+	err := flexMatchTmpl.Execute(&res, pod)
+	if err != nil {
+		log.Printf("Error executing flex match %s: %v\n", flexMatchTmpl.Name(), err)
+		return ""
+	}
+
+	log.Printf("Pod: %s, %s, Flex Match Result: %s\n", pod.Name, pod.GetName(), res.String())
+	return res.String()
+}
 
 func isMatch(pod corev1.Pod, rules MatchRules) bool {
 
@@ -63,6 +95,10 @@ func getInstrumentationRule(pod corev1.Pod) *InstrumentationRule {
 	defer config.mutex.Unlock()
 
 	fmt.Printf("Config: %v\n", config)
+
+	if config.FlexMatchTemplate != nil {
+		getFlexMatch(pod, config.FlexMatchTemplate)
+	}
 
 	for _, rule := range *config.InstrumentationConfig {
 		fmt.Printf("Checking rule: %s\n", rule.Name)
