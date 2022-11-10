c9c81d3876fcd420ba1af7056dba0f3e4de39d05
Martin Divis <mdivis@cisco.com>
2022-11-10 16:22:52 +0100

    Java Agent proxy support, initial DotNet support, fixes in MWH deployment

--- webhook/instrumentJava.go
+++ webhook/instrumentJava.go
@@ -1,6 +1,11 @@
 package main
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
 
 func javaInstrumentation(pod corev1.Pod, instrRule *InstrumentationRule) []patchOperation {
 	patchOps := []patchOperation{}
@@ -51,5 +56,95 @@ func javaInstrumentation(pod corev1.Pod, instrRule *InstrumentationRule) []patch
 
 	patchOps = append(patchOps, addJavaAgentVolume(pod, instrRule)...)
 
+	return patchOps
+}
+
+func addJavaEnvVar(pod corev1.Pod, instrRules *InstrumentationRule, containerIdx int) patchOperation {
+	return patchOperation{
+		Op:   "add",
+		Path: fmt.Sprintf("/spec/containers/%d/env/-", containerIdx),
+		Value: corev1.EnvVar{
+			Name:  instrRules.InjectionRules.JavaEnvVar,
+			Value: getJavaOptions(pod, instrRules),
+		},
+	}
+}
+
+func getJavaOptions(pod corev1.Pod, instrRules *InstrumentationRule) string {
+	javaOpts := " "
+
+	if config.ControllerConfig.UseProxy {
+		javaOpts += fmt.Sprintf("-Dappdynamics.http.proxyHost=%s ", config.ControllerConfig.ProxyHost)
+		javaOpts += fmt.Sprintf("-Dappdynamics.http.proxyPort=%s ", config.ControllerConfig.ProxyPort)
+	}
+
+	javaOpts += "-Dappdynamics.agent.accountAccessKey=$(APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY) "
+	javaOpts += "-Dappdynamics.agent.reuse.nodeName=true "
+	javaOpts += "-Dappdynamics.socket.collection.bci.enable=true "
+	javaOpts += "-javaagent:/opt/appdynamics-java/javaagent.jar "
+	javaOpts += instrRules.InjectionRules.JavaCustomConfig
+
+	return javaOpts
+}
+
+func addJavaAgentVolumeMount(pod corev1.Pod, instrRules *InstrumentationRule, containerIdx int) []patchOperation {
+	patchOps := []patchOperation{}
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: fmt.Sprintf("/spec/containers/%d/volumeMounts/-", containerIdx),
+		Value: corev1.VolumeMount{
+			MountPath: "/opt/appdynamics-java", //TODO
+			Name:      "appd-agent-repo-java",  //TODO
+		},
+	})
+	return patchOps
+}
+
+func addJavaAgentVolume(pod corev1.Pod, instrRules *InstrumentationRule) []patchOperation {
+	patchOps := []patchOperation{}
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: "/spec/volumes/-",
+		Value: corev1.Volume{
+			Name: "appd-agent-repo-java", //TODO
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
+		},
+	})
+	return patchOps
+}
+
+func addJavaAgentInitContainer(pod corev1.Pod, instrRules *InstrumentationRule) []patchOperation {
+	patchOps := []patchOperation{}
+	limCPU, _ := resource.ParseQuantity("200m")
+	limMem, _ := resource.ParseQuantity("75M")
+	reqCPU, _ := resource.ParseQuantity(instrRules.InjectionRules.ResourceReservation.CPU)
+	reqMem, _ := resource.ParseQuantity(instrRules.InjectionRules.ResourceReservation.Memory)
+
+	patchOps = append(patchOps, patchOperation{
+		Op:   "add",
+		Path: "/spec/initContainers/-",
+		Value: corev1.Container{
+			Name:            "appd-agent-attach-java", //TODO
+			Image:           instrRules.InjectionRules.Image,
+			Command:         []string{"cp", "-r", "/opt/appdynamics/.", "/opt/appdynamics-java"},
+			ImagePullPolicy: corev1.PullAlways, //TODO
+			Resources: corev1.ResourceRequirements{
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    limCPU,
+					corev1.ResourceMemory: limMem,
+				},
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    reqCPU,
+					corev1.ResourceMemory: reqMem,
+				},
+			},
+			VolumeMounts: []corev1.VolumeMount{{
+				MountPath: "/opt/appdynamics-java", //TODO
+				Name:      "appd-agent-repo-java",  //TODO
+			}},
+		},
+	})
 	return patchOps
 }
