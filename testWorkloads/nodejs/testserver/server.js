f2663d0db515f2c1cabef97e7a28c2f2914350c0
Martin Divis <mdivis@cisco.com>
2023-04-15 12:54:12 +0200

    nodejs otel test

--- testWorkloads/nodejs/testserver/server.js
+++ testWorkloads/nodejs/testserver/server.js
@@ -1,8 +1,11 @@
-const http = require("http");
+// const http = require("http");
+const express = require('express')
+const app = express()
 
 const host = '0.0.0.0';
 const port = 8000;
 
+/*
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("My test server!");
@@ -12,3 +15,32 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+*/
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/search', async (req, res) => {
+  await new Promise(r => setTimeout(r, randomDelay(500, 20)));
+  res.send('Hello API!')
+})
+
+app.get('/api/tool', async (req, res) => {
+  await new Promise(r => setTimeout(r, randomDelay(1000, 20)));
+  res.send('Hello Tool!')
+})
+
+app.get('/api/delete/:id/now', async (req, res) => {
+  await new Promise(r => setTimeout(r, randomDelay(1000, 20)));
+  res.send('Hello delete!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+function randomDelay(ms, randPct) {
+  const fluct = ms * randPct / 100 
+  return ms + Math.floor(Math.random() * fluct) - fluct/2;
+}
